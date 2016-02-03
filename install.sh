42bbb29b0b8f7f575e9d14e4cf648c83dc2f8a4a
Wilhelm Eklund <wille.eklund@gmail.com>
2016-02-03 11:15:10 +0100

    Updated URL to download Siege 3.1.3

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-wget http://www.joedog.org/pub/siege/siege-3.0.2.tar.gz
-tar zxvf siege-3.0.2.tar.gz
-cd siege-3.0.2
+wget http://download.joedog.org/siege/siege-3.1.3.tar.gz
+tar zxvf siege-3.1.3.tar.gz
+cd siege-3.1.3
 ./configure
 make
 cd ..
-mv siege-3.0.2/src/siege .
-rm -rf siege-3.0.2
-rm -rf siege-3.0.2.tar.gz
+mv siege-3.1.3/src/siege .
+rm -rf siege-3.1.3
+rm -rf siege-3.1.3.tar.gz
